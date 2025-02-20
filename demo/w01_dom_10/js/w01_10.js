1b41b1e28a56ea5268465155ed5fb5bb89fd2848
29261821 <gash29261821@gmail.com>
2025-02-20 21:14:07 +0800

    W01-P3: implement divide button

--- demo/w01_dom_10/js/w01_10.js
+++ demo/w01_dom_10/js/w01_10.js
@@ -16,18 +16,62 @@ console.log('currentResultOutput', currentResultOutput)
 console.log('currentCalculationOutput', currentCalculationOutput)
 
 function getUserInput() {
-  return parseInt(userInput.value) || 0 // ✅ 避免 NaN
+  return parseFloat(userInput.value) || 0 // ✅ 避免 NaN
+}
+function outputResult(result, text) {
+  currentResultOutput.textContent = result
+  currentCalculationOutput.textContent = text
 }
 
 function add() {
-  const operand1 = currentResult
+  const operand1 = parseFloat(currentResult)
   const operand2 = getUserInput()
-  currentResult = operand1 + operand2
-  currentResultOutput.textContent = currentResult
+  currentResult = parseFloat((operand1 + operand2).toFixed(2))
+  //currentResultOutput.textContent = currentResult
   const calText = `${operand1} + ${operand2}`
+  console.log(`${calText} = ${currentResult}`)
+  //currentCalculationOutput.textContent = calText
+  outputResult(currentResult, calText)
+}
+// 綁定事件
+addBtn.addEventListener('click', add)
+
+function subtract() {
+  const operand1 = parseFloat(currentResult)
+  const operand2 = getUserInput()
+  currentResult = parseFloat((operand1 - operand2).toFixed(2))
+  currentResultOutput.textContent = currentResult
+  const calText = `${operand1} - ${operand2}`
   console.log('calText', calText)
   currentCalculationOutput.textContent = calText
 }
-
 // 綁定事件
-addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+
+function multiply() {
+  const operand1 = parseFloat(currentResult)
+  const operand2 = getUserInput()
+  currentResult = parseFloat((operand1 * operand2).toFixed(2))
+  currentResultOutput.textContent = currentResult
+  const calText = `${operand1} * ${operand2}`
+  console.log('calText', calText)
+  currentCalculationOutput.textContent = calText
+}
+// 綁定事件
+multiplyBtn.addEventListener('click', multiply)
+
+function divide() {
+  const operand1 = parseFloat(currentResult)
+  const operand2 = getUserInput()
+  if (operand2 === 0) alert('cannot divide by 0')
+  else {
+    currentResult = parseFloat((operand1 / operand2).toFixed(2))
+    //currentResultOutput.textContent = currentResult
+    const calText = `${operand1} / ${operand2}`
+    console.log(`${calText} = ${currentResult}`)
+    //currentCalculationOutput.textContent = calText
+    outputResult(currentResult, calText)
+  }
+}
+// 綁定事件
+divideBtn.addEventListener('click', divide)
