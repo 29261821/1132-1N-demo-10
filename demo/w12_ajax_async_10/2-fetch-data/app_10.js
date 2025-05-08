0cd212ef8a5f47684f5b0d59947ebef4b7290755
29261821 <gash29261821@gmail.com>
2025-05-08 20:02:53 +0800

    W12-P1: use xhr to display a simple text

---
+++ demo/w12_ajax_async_10/2-fetch-data/app_10.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+  fetchData()
+})
+
+const fetchData = () => {
+  const xhr = new XMLHttpRequest()
+
+  console.log('xhr0', xhr)
+
+  xhr.open('GET', './api/simple.txt')
+  console.log('xhr', xhr)
+
+  xhr.onreadystatechange = () => {
+    console.log('xhr', xhr)
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p')
+      text.textContent = xhr.responseText
+      document.body.appendChild(text)
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      })
+    }
+  }
+
+  xhr.send()
+}
